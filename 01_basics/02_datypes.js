a7f91a4a48386db0dee260f1a8b3a5f89ecc9dc8
pujabhuj <76402204+pujabhuj@users.noreply.github.com>
2024-03-14 15:06:31 +0000

    datatype done

---
+++ 01_basics/02_datypes.js
@@ -0,0 +1,22 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3) // we are using node js , not browser
+
+let name="puja"
+let age = 5
+let isloged = true
+let state = null
+//-------Premitive Datatypes -------------------------------//
+// number =>2 to power 53
+//bigint
+//string ==>""
+//boolean ==> true/false
+//null ==> stanalone empty value
+//undefined ==> 
+//symbol ==>to find uniqness
+  
+
+//-------------Object --------------------------------/
+console.log(typeof undefined)//undefined
+console.log(typeof null)// object
+   
