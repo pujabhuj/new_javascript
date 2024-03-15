7da3bdc2e0e4dbfd799587a5b74b562029ded5ff
pujabhuj <76402204+pujabhuj@users.noreply.github.com>
2024-03-15 09:24:44 +0000

    conversion done

--- 01_basics/03_conversionopration.js
+++ 01_basics/03_conversionopration.js
@@ -4,7 +4,7 @@ console.log(typeof score)
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
-console.log(valueInNumber)
+console.log(valueInNumber)   
 
 //"33" ==>33
 //"33abc"  ==>NaN
